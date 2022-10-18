01713248a4f4c28da5d9e04de0e2a5d9e7f97976
Chris Kaltwasser <chris@chrisk.com>
2022-10-18 12:47:02 -0400

    Migrated <Switch> to <Routes> upgraded emotion imports.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "postagramimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
